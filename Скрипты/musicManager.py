991b7e2f752e51bd41d2ac33449153fd258a013b
sergeiovs <mr.serz98@mail.ru>
2022-09-18 00:16:37 -0700

    123

---
+++ Скрипты/musicManager.py
@@ -0,0 +1,17 @@
+import pygame as pg
+
+environmentsounds = [
+
+pg.mixer.Sound( 'Звуки/нло/ufo_bip_1.wav' ) , pg.mixer.Sound( 'Звуки/нло/ufo_bip_2.wav' ) , pg.mixer.Sound( 'Звуки/нло/ufo_bip_3.wav' ) ,
+
+pg.mixer.Sound( 'Звуки/нло/ufo_bip_4.wav' ) , pg.mixer.Sound( 'Звуки/нло/ufo_bip_5.wav' ) , pg.mixer.Sound( 'Звуки/нло/ufo_bip_6.wav' ) ,
+
+pg.mixer.Sound( 'Звуки/нло/ufo_bip_7.wav' ) , pg.mixer.Sound( 'Звуки/нло/ufo_bip_8.wav' ) , pg.mixer.Sound( 'Звуки/нло/ufo_bip_9.wav' ) ,
+
+pg.mixer.Sound( 'Звуки/нло/ufo_bip_10.wav' )]
+
+#soundtracks = [pg.mixer.music.load('Музыка/Damned.mp3')]
+
+soundtrack = pg.mixer.music.load('Музыка/main_menu soundtrack(Aghast - Sacrifice).mp3')
+
+environmentsound = environmentsounds[0]
